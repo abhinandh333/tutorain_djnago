70607caa55b9bf4aca29ff82c3e47cff74cfa8af
Abhinandh R Nair <abhinandhrnair333@gmail.com>
2026-02-01 05:15:45 +0530

    UI update

--- tutorain/settings.py
+++ tutorain/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--n@&bppbz6%vs@&rgtp_ko!!+f6_v4kjp!lord6gq_*ak%=n%n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.2', 'localhost', '127.0.0.1', 'testserver']
+ALLOWED_HOSTS = ['192.168.1.6', 'localhost', '127.0.0.1', 'testserver']
 
 
 
