35a721a57eabf820232e1575f9b4876fee825d70
Abhinandh R Nair <abhinandhrnair333@gmail.com>
2026-04-19 06:14:22 +0530

    fix robots.txt route

--- tutorain/settings.py
+++ tutorain/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--n@&bppbz6%vs@&rgtp_ko!!+f6_v4kjp!lord6gq_*ak%=n%n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tutoraindjnago-production.up.railway.app', '127.0.0.1', 'localhost','192.168.1.5',"tutorain.com","www.tutorain.com"]
+ALLOWED_HOSTS = ['tutoraindjnago-production.up.railway.app', '127.0.0.1', 'localhost','192.168.1.5','tutorain.com','www.tutorain.com']
 
 
 
