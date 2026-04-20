ab8de643dc88b72fa5f7c858990fc507531a1c5e
Abhinandh R Nair <abhinandhrnair333@gmail.com>
2026-04-20 10:11:00 +0530

    fix robots.txt route

--- tutorain/sitemaps.py
+++ tutorain/sitemaps.py
@@ -7,7 +7,7 @@ class StaticViewSitemap(Sitemap):
     changefreq = 'weekly'
 
     def items(self):
-        return ['home', 'findsub', 'contact', 'comingsoon']
+        return ['home', 'findsub', 'contact','login']
 
     def location(self, item):
         return reverse(item)
