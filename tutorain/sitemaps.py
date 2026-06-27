2ecfa845290d9be1b3491a54b80f7c59991a7364
Abhinandh R Nair <abhinandhrnair333@gmail.com>
2026-06-27 10:56:34 +0530

    Fix CSRF login cache issue

--- tutorain/sitemaps.py
+++ tutorain/sitemaps.py
@@ -7,7 +7,7 @@ class StaticViewSitemap(Sitemap):
     changefreq = 'weekly'
 
     def items(self):
-        return ['home', 'findsub', 'contact','login','maca']
+        return ['home', 'findsub', 'contact','login','maca','download']
 
     def location(self, item):
         return reverse(item)
